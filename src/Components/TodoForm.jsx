3ed50d3eab782c0d0fae591574d4448332408dbb
Mostak Khan <Mostak1993@gmail.com>
2024-01-28 19:36:15 +0000

    updates on todoapp

--- src/Components/TodoForm.jsx
+++ src/Components/TodoForm.jsx
@@ -20,7 +20,7 @@ export const TodoForm = ({ addTodo, removeAllTodos }) => {
       {/* Add Task Form */}
       <form onSubmit={handleSubmit} className="flex gap-3 justify-center">
         <input
-          className="outline rounded w-[500px] h-[50px] p-2 bg-white bg-gray-300 focus:outline-2 "
+          className="outline rounded w-[500px] h-[50px] p-2 bg-white focus:outline-2 "
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
